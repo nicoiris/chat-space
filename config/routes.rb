72dd9e5c4365a0107310b561ac633e201da1abd4
nicoiris <59913054+nicoiris@users.noreply.github.com>
2020-06-16 17:05:08 +0900

    Set a link to the new group creation screen

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
-  resouces :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create]
 end
